55099d257c93a1228ad40340894bbc8578f86a87
Das <sagnikdas98@hotmail.com>
2019-04-02 03:03:41 +0530

    Update round_robin_at.cpp

--- round_robin_at.cpp
+++ round_robin_at.cpp
@@ -5,62 +5,72 @@
 #include<fstream>
 
 using namespace std;
+
 class process{
     public :
-    int pid, bt, at;
-    static int quant, clk;
-    process(int a,int c, int b){
+    int pid, at, bt,tat,wt;
+    static int quant, clk, totalwt, totaltat;
+    process(int a,int b, int c){
     pid = a;
-    bt = b;
-    at = c;
+    at = b;
+    bt = c;
+	tat = -1;
+	wt = -bt;
     } 
+
+	void calcother(){
+		tat = clk - at;
+		wt += tat;
+		totaltat += tat;
+		totalwt += wt;
+	}
 };
+
 int process::clk=0;
+int process::totaltat=0;
+int process::totalwt=0;
 int process::quant=1;
+
 int main(){
 
-    vector<process> pp;
-    int ia,ib,ic,p=0,n,vi=1;
+    vector<process*> pp;
+    int ia,ib,ic,p=0,n,vi=0;
     process* temp;
 
-    std::fstream myfile("inpu.txt", std::ios_base::in);
+    fstream myfile("inpu.txt", ios_base::in);
     myfile>>n;
     for(int i = 0; i <n; i++){
         myfile>>ib>>ic;
-        pp.push_back(*(new process(i+1,ib,ic)));
+        pp.push_back((new process(i+1,ib,ic)));
     }
     myfile>>ia;
-    pp[0].quant = ia;
+    process::quant = ia;
 
-    queue<process> qq;
-    qq.push(pp[0]);
+    queue<process*> qq;
 
+    while(vi < n || !qq.empty()){
 
+        while((vi < n) && (process::clk >= pp[vi]->at)){
+			 qq.push(pp[vi]);
+            vi++;
+        }
 
-    cout<<"Process order:"<<endl;
+        temp = (qq.front());
 
-    while(!qq.empty()){
-
-        temp = &(qq.front());
-        cout<<(*temp).pid<<"-> ";
-        
-        if((*temp).bt >= (*temp).quant){
-            (*temp).bt-=(*temp).quant;
-            (*temp).clk+=(*temp).quant;
+        if((*temp).bt >= process::quant){
+            (*temp).bt-=process::quant;
+            process::clk+=process::quant;
             if((*temp).bt > 0) 
-                qq.push(*temp);
+                qq.push(temp);
             qq.pop();
         }
         else{
-            (*temp).clk+=(*temp).bt;
+            process::clk+=(*temp).bt;
+			(*temp).calcother();
             qq.pop();
-        }
-
-        while((vi < n) && ((*temp).clk >= pp[vi].at)){
-            qq.push(pp[vi]);
-            vi++;
-        }
+        }      
         
     }
+	cout<<"Avg TAT & Avg WT:"<<(process::totaltat)/n<<", "<<(process::totalwt)/n<<endl;
     return 0;
 }
